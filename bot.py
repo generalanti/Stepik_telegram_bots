b6dc7289bfe5098b26822b54e088a309f92afbbf
generalanti <generalanti@gmail.com>
2022-04-22 20:07:44 +0300

    make the bot to send message to primary account if a secret word was sent from any account

--- bot.py
+++ bot.py
@@ -1,33 +1,18 @@
 # --*-- encoding: utf-8 --*--
-# name: GOBfoGpsOgFv_bot
 
 import config
 import telebot
-import datetime
 
 bot = telebot.TeleBot(config.TOKEN)
 
 
+def secret_word(message):
+    return message.text == 'secret'
 
 
-
-@bot.message_handler(content_types=["text"])
-def speak(message):
-    date = message.date + 10800     # московское время (UTC + 3 часа в секундах)
-    chat_id = message.chat.id
-    user_id = message.from_user.id
-    name = message.from_user.first_name
-    lastname = message.from_user.last_name
-    username = message.from_user.username
-    message_text = message.text
-
-    msg = f"Я пробил информацию:\n\nId чата: {chat_id}\nId пользователя: {user_id}\nИмя: {name}\nФамилия: {lastname}\nПсевдоним: {username}\n\nТекст сообщения: {message_text}"
-    msg2 = datetime.datetime.utcfromtimestamp(date)
-
-    bot.send_message(message.chat.id, msg2)
+@bot.message_handler(func=secret_word)
+def echo_all(message):
+    bot.send_message(config.p_chat_id, 'введено секретное слово!')
 
 
 bot.polling(none_stop=True)
-
-
-
