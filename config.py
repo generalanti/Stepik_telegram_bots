0cfdd3d245aec754ce1ed284c74309d829a13c4a
generalanti <generalanti@gmail.com>
2022-04-22 20:04:47 +0300

    add vars for my two accounts

--- config.py
+++ config.py
@@ -1,4 +1,20 @@
 # token: 5365461689:AAEyp14F2yI98i53VoXx4BAnKyAiYmZ8MgU
-# name: GOBfoGpsOgFv_bot
+# bot name: GOBfoGpsOgFv
+
+# Primary account
+p_chat_id = 316075399
+p_user_id = 316075399
+p_name = 'Илья'
+p_lastname = 'Разуев'
+p_username = 'Ilya_impressive'
+
+
+# Secondary account
+s_chat_id = 5167752966
+s_user_id = 5167752966
+s_name = 'Ilya'
+s_lastname = 'Second'
+s_username = None
+
 
 TOKEN = '5365461689:AAEyp14F2yI98i53VoXx4BAnKyAiYmZ8MgU'
