b77499c02049b913ec81904a97a4b840e9b9ecdf
generalanti <generalanti@gmail.com>
2022-05-01 13:50:39 +0300

    Bot defines content types

--- bot.py
+++ bot.py
@@ -2,27 +2,15 @@
 
 import config
 import telebot
-import emoji
 
 bot = telebot.TeleBot(config.TOKEN)
 
 
-@bot.message_handler(content_types=["text"])
-def speak(message):
-
-    message_text = message.text + '... Ну и на кой хер ты это пишешь? Пожри-ка дерьма ' + emoji.emojize(':shit::shit::shit:', language='alias')
-
-    bot.send_message(message.chat.id, message_text)
+@bot.message_handler(content_types=['text', 'audio', 'document', 'photo', 'sticker', 'video', 'location', 'contact'])
+def start(message):
+    out_messages_ru = {'text': 'Это текст', 'audio': 'Это аудио', 'document': 'Это документ', 'photo': 'Это фото',
+                       'sticker': 'Это стикер', 'video': 'Это видео', 'location': 'Это геопозиция', 'contact': 'Это контакт'}
+    bot.send_message(message.chat.id, out_messages_ru[message.content_type])
 
 
 bot.polling(none_stop=True)
-
-
-
-
-# Если введены команды из списка, то выполняется функция
-# @bot.message_handler(commands=['start', 'end'])
-# def start(message):
-#     bot.send_message(message.chat.id, 'Hi!')
-
-
