64092edf8cf0067f0d07de47dad347fc99529811
generalanti <generalanti@gmail.com>
2022-05-01 16:22:52 +0300

    Bot sends a photo or video to administrator if another user sends it. Otherwise he sends a message of wrong content type.
    add new snippets

--- bot.py
+++ bot.py
@@ -7,8 +7,17 @@ import telebot
 bot = telebot.TeleBot(config.TOKEN)
 
 
-@bot.message_handler(content_types=['sticker'])
+@bot.message_handler(content_types=['video', 'photo'])
+@bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_sticker(message.chat.id, message.sticker.file_id)
+    if message.content_type == 'photo':
+        bot.send_photo(5167752966, message.photo[0].file_id)
+    elif message.content_type == 'video':
+        bot.send_video(5167752966, message.video.file_id)
+
+
+@bot.message_handler(func=lambda m: True)
+def chat(message):
+    bot.send_message(5167752966, 'Это не видео и не фото :(')
 
 bot.polling(none_stop=True)
